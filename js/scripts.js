8ec9710d42d3cb5ae05a2f658580e2f8a794819d
April Peng <helloaprilpeng@gmail.com>
2016-07-27 19:53:08 -0700

    update js functionality to value size selected and count number of toppings selected. add form content and headers to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,42 @@
 //business logic
-function Pizza(topping1, topping2, topping3) {
-  this.topping1 = topping1;
-  this.topping2 = topping2;
-  this.topping3 = topping3;
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = [];
 }
 
-Pizza.prototype.Order = function()  {}
+Pizza.prototype.addToTotalCost = function()  {
+  return (this.toppings + this.size);
+  alert("hi");
+}
+
 
 //user interface logic
+$(function() {
+  var newPizza = new Pizza();
+
+  $("#submit").click(function() {
+    var size = parseInt($("#size").val());
+    console.log(size);
+    if (size === 1) {
+      this.size = 6;
+    }
+    if (size === 2) {
+      this.size = 8;
+    }
+
+    var toppings = document.getElementsByTagName("input"),
+    count = 0;
+    for (var i=0; i<toppings.length; i++) {
+      if (toppings[i].type === "checkbox" && toppings[i].checked === true) {
+        count++;
+      }
+    }
+    console.log(count);
+    this.toppings = count * 0.5
+
+    newPizza.cost = newPizza.addToTotalCost();
+    $("#pizza-cost").text("Alright, brother - Great selections! Your total comes to " + newPizza.cost + "! Your insanely hot, delicious pizza will be ready for pickup and hearty consumption in 15 minutes!")
+    
+  });
+
+});
